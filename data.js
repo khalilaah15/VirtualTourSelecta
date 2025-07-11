0e9e29f7ae7c02f367ab507f44800e8d3dda2107
khalilaah15 <khalilullahnuraini@gmail.com>
2025-07-11 21:23:16 +0700

    update lagi ygy

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const tours = [
     id: 1,
     name: "Kolam Renang",
     description: "Kolam renang Selecta merupakan salah satu daya tarik utama di Taman Rekreasi Selecta. Terletak di dataran tinggi dengan udara sejuk khas pegunungan, kolam ini memiliki air yang dingin dan menyegarkan.",
-    image: "/assets/KolamRenang.JPG",
+    image: "/assets/KolamRenang.jpg",
     tourUrl: "/tours/KolamRenang.html"
   },
   {
@@ -17,7 +17,7 @@ export const tours = [
     id: 3,
     name: "Tirta Kamandanu Waterpark",
     description: "Tirta Kamandanu Waterpark menawarkan kolam renang dengan sumber air yang langsung berasal dari mata air pegunungan, yaitu Sumber Jobranti 1 & 2 serta Sumber Sri Agung, sehingga terasa segar dan alami.",
-    image: "/assets/TirtaKamandanuWaterpark.JPG",
+    image: "/assets/TirtaKamandanuWaterpark.jpg",
     tourUrl: "/tours/TirtaKamandanuWaterpark.html"
   },
   {
@@ -31,56 +31,56 @@ export const tours = [
     id: 5,
     name: "Sky Bike",
     description: "Sky Bike merupakan wahana unik di Taman Rekreasi Selecta yang memungkinkan pengunjung mengayuh sepeda di atas rel yang tergantung di udara.",
-    image: "/assets/SkyBike.JPG",
+    image: "/assets/SkyBike.jpg",
     tourUrl: "/tours/SkyBike.html"
   },
   {
     id: 6,
     name: "Garden Tram",
     description: "Garden Tram (Garden Trem) adalah wahana kereta mini yang membawa pengunjung berkeliling area taman bunga dan lanskap indah di Taman Rekreasi Selecta.",
-    image: "/assets/GardenTram.JPG",
+    image: "/assets/GardenTram.jpg",
     tourUrl: "/tours/GardenTram.html"
   },
   {
     id: 7,
     name: "Mobil Ayun",
     description: "Mobil Ayun di Taman Rekreasi Selecta adalah salah satu wahana permainan yang memberikan sensasi goyangan naik-turun dan maju-mundur secara ritmis dalam lintasan melingkar. Wahana ini cocok untuk segala usia.",
-    image: "/assets/MobilAyun.JPG",
+    image: "/assets/MobilAyun.jpg",
     tourUrl: "/tours/MobilAyun.html"
   },
   {
     id: 8,
     name: "Paddle Boat",
     description: "Paddle boat atau sepeda air adalah salah satu wahana favorit di Selecta, di mana pengunjung bisa mengayuh perahu seperti sepeda di atas kolam yang tenang sambil menikmati lanskap taman bunga dan pepohonan.",
-    image: "/assets/PadleBoat.JPG",
+    image: "/assets/PadleBoat.jpg",
     tourUrl: "/tours/PadleBoat.html"
   },
   {
     id: 9,
     name: "Mini Bumper Car",
     description: "Mini Bumper Car adalah wahana permainan mobil listrik berukuran kecil yang dirancang untuk anak-anak. Di wahana ini, pengunjung dapat mengendarai mobil-mobil mini dan saling bertabrakan dalam arena yang aman dan empuk.",
-    image: "/assets/MiniBumperCar.JPG",
+    image: "/assets/MiniBumperCar.jpg",
     tourUrl: "/tours/MiniBumperCar.html"
   },
   {
     id: 10,
     name: "4D Cinema",
     description: "4D Cinema adalah wahana bioskop interaktif yang memadukan teknologi visual tiga dimensi (3D) dengan efek fisik tambahan seperti gerakan kursi, hembusan angin, semprotan air, dan efek suara menggelegar.",
-    image: "/assets/Cinema4D.JPG",
+    image: "/assets/Cinema4D.jpg",
     tourUrl: "/tours/Cinema4D.html"
   },
   {
     id: 11,
     name: "Dino Ride",
     description: "Dino Ride merupakan wahana permainan berupa kendaraan listrik berbentuk dinosaurus berwarna-warni yang dapat dinaiki oleh anak-anak maupun remaja dan dikendarai di area khusus yang disediakan.",
-    image: "/assets/DinoRide.JPG",
+    image: "/assets/DinoRide.jpg",
     tourUrl: "/tours/DinoRide.html"
   },
   {
     id: 12,
     name: "Family Coaster",
     description: "Family Coaster adalah salah satu wahana favorit di Taman Rekreasi Selecta. Wahana ini berupa roller coaster ringan yang didesain khusus untuk dinikmati seluruh anggota keluarga, termasuk anak-anak.",
-    image: "/assets/FamilyCoaster.JPG",
+    image: "/assets/FamilyCoaster.jpg",
     tourUrl: "/tours/FamilyCoaster.html"
   },
 ];

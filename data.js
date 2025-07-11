d952546d6bce6ba51680ec843a7f5b1cb83a5e55
khalilaah15 <khalilullahnuraini@gmail.com>
2025-07-11 21:08:13 +0700

    kha sangat berusaha

--- data.js
+++ data.js
@@ -4,83 +4,83 @@ export const tours = [
     name: "Kolam Renang",
     description: "Kolam renang Selecta merupakan salah satu daya tarik utama di Taman Rekreasi Selecta. Terletak di dataran tinggi dengan udara sejuk khas pegunungan, kolam ini memiliki air yang dingin dan menyegarkan.",
     image: "/assets/KolamRenang.JPG",
-    tourUrl: "tours/KolamRenang.html"
+    tourUrl: "/tours/KolamRenang.html"
   },
   {
     id: 2,
     name: "Bianglala",
     description: "Bianglala di Taman Rekreasi Selecta adalah salah satu wahana favorit yang menawarkan pengalaman melihat panorama indah, luas, tak terbabas dengan pegunungan dan taman bunga dari ketinggian.",
     image: "/assets/Bianglala.jpg",
-    tourUrl: "tours/Bianglala.html"
+    tourUrl: "/tours/Bianglala.html"
   },
   {
     id: 3,
     name: "Tirta Kamandanu Waterpark",
     description: "Tirta Kamandanu Waterpark menawarkan kolam renang dengan sumber air yang langsung berasal dari mata air pegunungan, yaitu Sumber Jobranti 1 & 2 serta Sumber Sri Agung, sehingga terasa segar dan alami.",
     image: "/assets/TirtaKamandanuWaterpark.JPG",
-    tourUrl: "tours/TirtaKamandanuWaterpark.html"
+    tourUrl: "/tours/TirtaKamandanuWaterpark.html"
   },
   {
     id: 4,
     name: "Flying Fox",
     description: "Wahana meluncur dari ketinggian dengan menggunakan tali dan katrol yang menggantung di atas kabel baja, sambil menikmati panorama taman bunga dan pegunungan dari udara.",
     image: "/assets/FlyingFox.jpeg",
-    tourUrl: "tours/FlyingFox.html"
+    tourUrl: "/tours/FlyingFox.html"
   },
   {
     id: 5,
     name: "Sky Bike",
     description: "Sky Bike merupakan wahana unik di Taman Rekreasi Selecta yang memungkinkan pengunjung mengayuh sepeda di atas rel yang tergantung di udara.",
     image: "/assets/SkyBike.JPG",
-    tourUrl: "tours/SkyBike.html"
+    tourUrl: "/tours/SkyBike.html"
   },
   {
     id: 6,
     name: "Garden Tram",
     description: "Garden Tram (Garden Trem) adalah wahana kereta mini yang membawa pengunjung berkeliling area taman bunga dan lanskap indah di Taman Rekreasi Selecta.",
     image: "/assets/GardenTram.JPG",
-    tourUrl: "tours/GardenTram.html"
+    tourUrl: "/tours/GardenTram.html"
   },
   {
     id: 7,
     name: "Mobil Ayun",
     description: "Mobil Ayun di Taman Rekreasi Selecta adalah salah satu wahana permainan yang memberikan sensasi goyangan naik-turun dan maju-mundur secara ritmis dalam lintasan melingkar. Wahana ini cocok untuk segala usia.",
     image: "/assets/MobilAyun.JPG",
-    tourUrl: "tours/MobilAyun.html"
+    tourUrl: "/tours/MobilAyun.html"
   },
   {
     id: 8,
     name: "Paddle Boat",
     description: "Paddle boat atau sepeda air adalah salah satu wahana favorit di Selecta, di mana pengunjung bisa mengayuh perahu seperti sepeda di atas kolam yang tenang sambil menikmati lanskap taman bunga dan pepohonan.",
     image: "/assets/PadleBoat.JPG",
-    tourUrl: "tours/PadleBoat.html"
+    tourUrl: "/tours/PadleBoat.html"
   },
   {
     id: 9,
     name: "Mini Bumper Car",
     description: "Mini Bumper Car adalah wahana permainan mobil listrik berukuran kecil yang dirancang untuk anak-anak. Di wahana ini, pengunjung dapat mengendarai mobil-mobil mini dan saling bertabrakan dalam arena yang aman dan empuk.",
     image: "/assets/MiniBumperCar.JPG",
-    tourUrl: "tours/MiniBumperCar.html"
+    tourUrl: "/tours/MiniBumperCar.html"
   },
   {
     id: 10,
     name: "4D Cinema",
     description: "4D Cinema adalah wahana bioskop interaktif yang memadukan teknologi visual tiga dimensi (3D) dengan efek fisik tambahan seperti gerakan kursi, hembusan angin, semprotan air, dan efek suara menggelegar.",
     image: "/assets/Cinema4D.JPG",
-    tourUrl: "tours/Cinema4D.html"
+    tourUrl: "/tours/Cinema4D.html"
   },
   {
     id: 11,
     name: "Dino Ride",
     description: "Dino Ride merupakan wahana permainan berupa kendaraan listrik berbentuk dinosaurus berwarna-warni yang dapat dinaiki oleh anak-anak maupun remaja dan dikendarai di area khusus yang disediakan.",
     image: "/assets/DinoRide.JPG",
-    tourUrl: "tours/DinoRide.html"
+    tourUrl: "/tours/DinoRide.html"
   },
   {
     id: 12,
     name: "Family Coaster",
     description: "Family Coaster adalah salah satu wahana favorit di Taman Rekreasi Selecta. Wahana ini berupa roller coaster ringan yang didesain khusus untuk dinikmati seluruh anggota keluarga, termasuk anak-anak.",
     image: "/assets/FamilyCoaster.JPG",
-    tourUrl: "tours/FamilyCoaster.html"
+    tourUrl: "/tours/FamilyCoaster.html"
   },
 ];
